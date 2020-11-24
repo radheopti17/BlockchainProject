6af26553e26bd82c2975c181dabfb24adba028d7
Radha Raman <radheopti17@gmail.com>
2020-11-24 09:44:07 +0530

    Created the structure of the project

---
+++ BlockchainProject/level1/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
